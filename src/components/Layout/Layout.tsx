6dd5060e756f22fb1531bb9981a47a4524a491d8
rar-files <dwidlak@outlook.com>
2021-11-21 01:42:07 +0100

    Re-organizeProjectStructure_AddRouter

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+import Views from '../Views'
+
+const MainPage : FC = () => {
+    return(
+        <>
+            <Views/>
+        </>
+    )
+}
+
+export default MainPage;
---
+++ src/components/Views/index.tsx
@@ -0,0 +1,14 @@
+import {FC} from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const Views : FC = () => {
+    return(
+        <>
+            <Routes>
+                <Route path="/"/>
+            </Routes>
+        </>
+    )
+}
+
+export default Views;
