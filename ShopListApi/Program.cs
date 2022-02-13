53e0b76df7da24e0b1209df59c8ab1021202f8e3
rar-files <dwidlak@outlook.com>
2022-02-13 07:12:47 +0100

    ConfigureApp

--- ShopListApi/Program.cs
+++ ShopListApi/Program.cs
@@ -1,19 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using ShopListApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ShopListDBContext>(options => 
+    options
+        .UseNpgsql(builder.Configuration.GetConnectionString("ShopListDBContext"))
+        .UseSnakeCaseNamingConvention()
+        .UseLoggerFactory(LoggerFactory.Create(b => b.AddConsole()))
+        .EnableSensitiveDataLogging()
+);
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
